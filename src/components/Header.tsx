bb35cd9a13057033ea2adafac152a3107e18b786
kishan2624 <ftd10822422@dseu.ac.in>
2025-04-06 19:18:35 +0530

    fix deploy error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "./Navbar";
 
 export const Header = () => {
   return (
-    <header className=" p-4 md:p-6 items-center dark:bg-slate-900 bg-slate-50">
+    <header className="sticky top-0 z-50 flex p-4 md:p-6 items-center dark:bg-slate-900 bg-slate-50 shadow-md">
       <div className="flex justify-between container mx-auto">
         {/* Logo */}
         <div className="text-3xl font-bold flex items-center space-x-2 text-slate-700  dark:text-slate-200">
